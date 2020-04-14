30334073f8daeb2afee4da269d02d81a86bb0c2d
“Zach <z_wood@baylor.edu>
2020-04-14 05:04:28 +0000

    added lesson submission

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
   root 'static_pages#index'  
   resources :courses, only: [:index, :show]
   namespace :instructor do 
-    resources :courses, only: [:new, :create, :show] do 
+    resources :sections, only: [] do 
+      resources :lessons, only: [:new, :create]
+    end
+    resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:new, :create]
     end
   end
