f7e22d48cde718a8efbcd52d8ea23da6122f5251
“Zach <z_wood@baylor.edu>
2020-04-20 19:49:46 +0000

    authenticated enrolled user for lesson page access

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,11 +1,25 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_enrolled_for_current_course, only: [:show]
+
   def show
   end
 
   private
 
+  def require_enrolled_for_current_course
+    if current_user.enrolled_in?(current_lesson.section.course) == false
+      # render plain: "Unauthorized", status: :unauthorized
+      redirect_to course_path(current_lesson.section.course), alert: 'You are not enrolled in this course! :('
+    end
+  end
+
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
+
+  # def current_course
+  #   @current_course ||= Course.find(params[:course_id])
+  # end
 end
